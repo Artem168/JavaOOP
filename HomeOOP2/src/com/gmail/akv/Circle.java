0b175c067a657edbfbe7962d5aa0e0654e10e5fd
Artem168 <78231040+Artem168@users.noreply.github.com>
2021-03-03 18:54:28 +0200

    Add files via upload

---
+++ HomeOOP2/src/com/gmail/akv/Circle.java
@@ -0,0 +1,54 @@
+package com.gmail.akv;
+
+public class Circle extends Shape {
+	private Point a;
+	private Point b;
+
+	public Circle(Point a, Point b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	public Circle() {
+		super();
+	}
+
+	public Point getA() {
+		return a;
+	}
+
+	public void setA(Point a) {
+		this.a = a;
+	}
+
+	public Point getB() {
+		return b;
+	}
+
+	public void setB(Point b) {
+		this.b = b;
+	}
+
+	
+
+	@Override
+	public double getPerimetr() {
+		double radius = Math
+				.sqrt((a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()));
+		return 2 * Math.PI * radius;
+	}
+
+	@Override
+	public double getArea() {
+		double radius = Math
+				.sqrt((a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()));
+		return Math.PI * radius * radius;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [a=" + a + ", b=" + b + "]";
+	}
+
+}
---
+++ HomeOOP2/src/com/gmail/akv/Triangle.java
@@ -0,0 +1,74 @@
+package com.gmail.akv;
+
+public class Triangle extends Shape {
+	private Point a;
+	private Point b;
+	private Point c;
+
+	public Triangle(Point a, Point b, Point c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public Triangle() {
+		super();
+	}
+
+	public Point getA() {
+		return a;
+	}
+
+	public void setA(Point a) {
+		this.a = a;
+	}
+
+	public Point getB() {
+		return b;
+	}
+
+	public void setB(Point b) {
+		this.b = b;
+	}
+
+	public Point getC() {
+		return c;
+	}
+
+	public void setC(Point c) {
+		this.c = c;
+	}
+
+	
+
+	@Override
+	public double getPerimetr() {
+		double sideOne = Math
+				.sqrt((a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()));
+		double sideTwo = Math
+				.sqrt((b.getX() - c.getX()) * (b.getX() - c.getX()) + (b.getY() - c.getY()) * (b.getY() - c.getY()));
+		double sideThree = Math
+				.sqrt((c.getX() - a.getX()) * (c.getX() - a.getX()) + (c.getY() - a.getY()) * (c.getY() - a.getY()));
+		double p = (sideOne + sideTwo + sideThree) / 2;
+		return sideOne + sideTwo + sideThree;
+	}
+
+	@Override
+	public double getArea() {
+		double sideOne = Math
+				.sqrt((a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()));
+		double sideTwo = Math
+				.sqrt((b.getX() - c.getX()) * (b.getX() - c.getX()) + (b.getY() - c.getY()) * (b.getY() - c.getY()));
+		double sideThree = Math
+				.sqrt((c.getX() - a.getX()) * (c.getX() - a.getX()) + (c.getY() - a.getY()) * (c.getY() - a.getY()));
+		double p = (sideOne + sideTwo + sideThree) / 2;
+		return Math.sqrt(p * (p - sideOne) * (p - sideTwo) * (p - sideThree));
+	}
+
+	@Override
+	public String toString() {
+		return "Triangle [a=" + a + ", b=" + b + ", c=" + c + "]";
+	}
+
+}
